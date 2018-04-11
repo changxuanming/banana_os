6c55282a2fa5f8e2c16d403df551e50fe99c9e83
changxuanming <nj.xuanming@gmail.com>
2018-04-11 23:03:53 +0800

    对界面的修改

--- src/bootpack.c
+++ src/bootpack.c
@@ -2,56 +2,69 @@
 
 void io_hlt(void);
 void io_cli(void);
-void io_out8(int port,int data); // port：设别号（硬件规定）
+void io_out8(int port, int data);// port：设别号（硬件规定）
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
 
-
-void init_palette(void );//初始化调色板
+void init_palette(void);//初始化调色板
 void set_palette(int start, int end, unsigned char *rgb);//设置调色板颜色
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 
-
-#define COL8_BLACK		0
-#define COL8_RED		1
-#define COL8_GREEN		2
-#define COL8_YELLOW		3
-#define COL8_BLUE		4
-#define COL8_PURPLE		5
-#define COL8_blue		6
-#define COL8_WHITE		7
-#define COL8_GRAY		8
-#define COL8_red		9
-#define COL8_green		10
-#define COL8_yellow		11
-#define COL8_cyan		12
-#define COL8_purple		13
-#define COL8_Lblue		14
-#define COL8_gray		15
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
 
 void HariMain(void)
 {
+	char *vram;
 	int xsize, ysize;
-	char * vram;
-	
+
 	init_palette(); // 初始化调色板设备
-	vram = (char*) 0xa0000;//显卡内存地址
-	
-	xsize=320;
-	ysize=200;
-	
-	boxfill8(vram, xsize, COL8_gray, 20, 20, 100, 100);
-	
-	for(;;){
+	vram = (char *) 0xa0000;//显卡内存地址
+	xsize = 320;
+	ysize = 200;
+
+	/* 根据 0xa0000 + x + y * 320 计算坐标 8*/
+	boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
+
+	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+
+	for (;;) {
 		io_hlt();
 	}
-
 }
 
-void init_palette(void){
-	static unsigned char table_rgb[16*3] = {
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
 		0x00, 0x00, 0x00,	/*  0:黑 */
-		0xff, 0x00, 0x00,	/*  1:亮红 */
+		0xff, 0x00, 0x00,	/*  1:梁红 */
 		0x00, 0xff, 0x00,	/*  2:亮绿 */
 		0xff, 0xff, 0x00,	/*  3:亮黄 */
 		0x00, 0x00, 0xff,	/*  4:亮蓝 */
@@ -67,34 +80,35 @@ void init_palette(void){
 		0x00, 0x84, 0x84,	/* 14:浅暗蓝 */
 		0x84, 0x84, 0x84	/* 15:暗灰 */
 	};
-	set_palette(0,15,table_rgb);
+	set_palette(0, 15, table_rgb);
 	return;
+
+	/* C语言中的static char语句只能用于数据，相当于汇编中的DB指令 */
 }
 
-void set_palette(int start, int end, unsigned char *rgb){
+void set_palette(int start, int end, unsigned char *rgb)
+{
 	int i, eflags;
-	eflags = io_load_eflags();//保存中断标志位
-	
-	io_cli();	//中断许可标志为0，禁止中断
-	io_out8(0x03c8,start);
-	for(i = start; i <= end; i++){
-		io_out8(0x03c9, rgb[0]/4);
-		io_out8(0x03c9, rgb[1]/4);
-		io_out8(0x03c9, rgb[2]/4);
+	eflags = io_load_eflags();	/* 记录中断许可标志的值 */
+	io_cli(); 					/* 将中断许可标志置为0,禁止中断 */
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
 		rgb += 3;
 	}
-	io_store_eflags(eflags);
+	io_store_eflags(eflags);	/* 复原中断许可标志 */
 	return;
 }
 
-void boxfill8(unsigned char *vram, int xsize, unsigned char c,int x0, int y0, int x1, int y1)
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
 {
 	int x, y;
-	for(y = y0; y<y1; y++){
-		for(x = x0; x<x1; x++){
+	for (y = y0; y <= y1; y++) {
+		for (x = x0; x <= x1; x++)
 			/* 根据 0xa0000 + x + y * xsize 计算坐标 8*/
-			vram[y*xsize+x]=c;
-		}
+			vram[y * xsize + x] = c;
 	}
 	return;
 }
