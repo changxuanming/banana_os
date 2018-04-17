a36d2792a9c5d9681ff47e66f3f1a4ece26e87fe
changxuanming <nj.xuanming@gmail.com>
2018-04-17 23:37:08 +0800

    分割编译

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,58 +1,21 @@
-#include<stdio.h>
+/* bootpack�̃��C�� */
 
-/* C语言的函数在汇编中实现 */
-
-void io_hlt(void);
-void io_cli(void);
-void io_out8(int port, int data);// port：设别号（硬件规定）
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
-
-void init_palette(void);//初始化调色板
-void set_palette(int start, int end, unsigned char *rgb);//设置调色板颜色
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-void init_screen8(char *vram, int x, int y);
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font);//界面上显示一个字符
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);//在界面上显示一段文字
-void init_mouse_cursor8(char *mouse, char bc);
-void putblock8_8(char *vram, int vxsize, int pxsize,
-	int pysize, int px0, int py0, char *buf, int bxsize);
-
-#define COL8_000000		0
-#define COL8_FF0000		1
-#define COL8_00FF00		2
-#define COL8_FFFF00		3
-#define COL8_0000FF		4
-#define COL8_FF00FF		5
-#define COL8_00FFFF		6
-#define COL8_FFFFFF		7
-#define COL8_C6C6C6		8
-#define COL8_840000		9
-#define COL8_008400		10
-#define COL8_848400		11
-#define COL8_000084		12
-#define COL8_840084		13
-#define COL8_008484		14
-#define COL8_848484		15
-
-struct BOOTINFO {
-	char cyls, leds, vmode, reserve;
-	short scrnx, scrny;
-	char *vram;
-};
+#include "bootpack.h"
+#include <stdio.h>
 
 void HariMain(void)
 {
-	struct BOOTINFO *binfo = (struct BOOTINFO*) 0x0ff0;
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256];
 	int mx, my;
-	
-	init_palette(); // 初始化调色板设备
-	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);//初始化界面背景
-	
-	mx = (binfo->scrnx - 16) / 2; /* 画面中央坐标的计算 */
+
+	init_gdtidt();
+	init_palette();// ���n��?�F��??
+	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);//���n���E�ʔw�i
+	mx = (binfo->scrnx - 16) / 2;  /* ��ʒ�����?�I?�Z */
 	my = (binfo->scrny - 28 - 16) / 2;
-	init_mouse_cursor8(mcursor, COL8_008484);
+	init_mouse_cursor8(mcursor, COL8_008484);/*?mcursor???�`???�F*/
+	/*�ݓ���ʒu?���꘢���f?*/
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
@@ -61,164 +24,3 @@ void HariMain(void)
 		io_hlt();
 	}
 }
-
-void init_palette(void)
-{
-	static unsigned char table_rgb[16 * 3] = {
-		0x00, 0x00, 0x00,	/*  0:黑 */
-		0xff, 0x00, 0x00,	/*  1:梁红 */
-		0x00, 0xff, 0x00,	/*  2:亮绿 */
-		0xff, 0xff, 0x00,	/*  3:亮黄 */
-		0x00, 0x00, 0xff,	/*  4:亮蓝 */
-		0xff, 0x00, 0xff,	/*  5:亮紫 */
-		0x00, 0xff, 0xff,	/*  6:浅亮蓝 */
-		0xff, 0xff, 0xff,	/*  7:白 */
-		0xc6, 0xc6, 0xc6,	/*  8:亮灰 */
-		0x84, 0x00, 0x00,	/*  9:暗红 */
-		0x00, 0x84, 0x00,	/* 10:暗绿 */
-		0x84, 0x84, 0x00,	/* 11:暗黄 */
-		0x00, 0x00, 0x84,	/* 12:暗青 */
-		0x84, 0x00, 0x84,	/* 13:暗紫 */
-		0x00, 0x84, 0x84,	/* 14:浅暗蓝 */
-		0x84, 0x84, 0x84	/* 15:暗灰 */
-	};
-	set_palette(0, 15, table_rgb);
-	return;
-
-	/* C语言中的static char语句只能用于数据，相当于汇编中的DB指令 */
-}
-
-void set_palette(int start, int end, unsigned char *rgb)
-{
-	int i, eflags;
-	eflags = io_load_eflags();	/* 记录中断许可标志的值 */
-	io_cli(); 					/* 将中断许可标志置为0,禁止中断 */
-	io_out8(0x03c8, start);
-	for (i = start; i <= end; i++) {
-		io_out8(0x03c9, rgb[0] / 4);
-		io_out8(0x03c9, rgb[1] / 4);
-		io_out8(0x03c9, rgb[2] / 4);
-		rgb += 3;
-	}
-	io_store_eflags(eflags);	/* 复原中断许可标志 */
-	return;
-}
-
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
-{
-	int x, y;
-	for (y = y0; y <= y1; y++) {
-		for (x = x0; x <= x1; x++)
-			/* 根据 0xa0000 + x + y * xsize 计算坐标 8*/
-			vram[y * xsize + x] = c;
-	}
-	return;
-}
-
-//界面函数
-void init_screen8(char *vram, int x, int y){
-	
-	boxfill8(vram, x, COL8_008484,  0,     0,      x -  1, y - 29);
-	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
-	boxfill8(vram, x, COL8_FFFFFF,  0,     y - 27, x -  1, y - 27);
-	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 26, x -  1, y -  1);
-
-	boxfill8(vram, x, COL8_FFFFFF,  3,     y - 24, 59,     y - 24);
-	boxfill8(vram, x, COL8_FFFFFF,  2,     y - 24,  2,     y -  4);
-	boxfill8(vram, x, COL8_848484,  3,     y -  4, 59,     y -  4);
-	boxfill8(vram, x, COL8_848484, 59,     y - 23, 59,     y -  5);
-	boxfill8(vram, x, COL8_000000,  2,     y -  3, 59,     y -  3);
-	boxfill8(vram, x, COL8_000000, 60,     y - 24, 60,     y -  3);
-
-	boxfill8(vram, x, COL8_848484, x - 47, y - 24, x -  4, y - 24);
-	boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y -  4);
-	boxfill8(vram, x, COL8_FFFFFF, x - 47, y -  3, x -  4, y -  3);
-	boxfill8(vram, x, COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
-	return;
-}
-
-//单个字符显示函数
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
-{
-	int i;
-	char *p, d /* data */;
-	for (i = 0; i < 16; i++) {
-		p = vram + (y + i) * xsize + x;
-		d = font[i];//逐行扫描找出标记的位
-		if ((d & 0x80) != 0) { p[0] = c; }//'c' =='color'
-		if ((d & 0x40) != 0) { p[1] = c; }
-		if ((d & 0x20) != 0) { p[2] = c; }
-		if ((d & 0x10) != 0) { p[3] = c; }
-		if ((d & 0x08) != 0) { p[4] = c; }
-		if ((d & 0x04) != 0) { p[5] = c; }
-		if ((d & 0x02) != 0) { p[6] = c; }
-		if ((d & 0x01) != 0) { p[7] = c; }
-	}
-	return;
-}
-
-//显示一段话
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s)
-{
-	extern char hankaku[4096];
-	/* C语言中，字符串都是以0x00结尾 */
-	for (; *s != 0x00; s++) {
-		putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
-		x += 8;
-	}
-	
-	return;
-}
-
-void init_mouse_cursor8(char *mouse, char bc)
-{
-	static char cursor[16][16] = {
-		"**************..",
-		"*OOOOOOOOOOO*...",
-		"*OOOOOOOOOO*....",
-		"*OOOOOOOOO*.....",
-		"*OOOOOOOO*......",
-		"*OOOOOOO*.......",
-		"*OOOOOOO*.......",
-		"*OOOOOOOO*......",
-		"*OOOO**OOO*.....",
-		"*OOO*..*OOO*....",
-		"*OO*....*OOO*...",
-		"*O*......*OOO*..",
-		"**........*OOO*.",
-		"*..........*OOO*",
-		"............*OO*",
-		".............***"
-	};
-	int x, y;
-
-	for (y = 0; y < 16; y++) {
-		for (x = 0; x < 16; x++) {
-			if (cursor[y][x] == '*') {
-				mouse[y * 16 + x] = COL8_000000;
-			}
-			if (cursor[y][x] == 'O') {
-				mouse[y * 16 + x] = COL8_FFFFFF;
-			}
-			if (cursor[y][x] == '.') {
-				mouse[y * 16 + x] = bc;
-			}
-		}
-	}
-	return;
-}
-
-void putblock8_8(char *vram, int vxsize, int pxsize,
-	int pysize, int px0, int py0, char *buf, int bxsize)
-{
-	int x, y;
-	for (y = 0; y < pysize; y++) {
-		for (x = 0; x < pxsize; x++) {
-			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
-		}
-	}
-	return;
-}
-
-
-
