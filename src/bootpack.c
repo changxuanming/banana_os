898b39567400fc3a41fbf1bed29a09ecefbb0bb1
changxuanming <nj.xuanming@gmail.com>
2018-04-18 09:52:37 +0800

    添加中文批注

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,4 +1,4 @@
-/* bootpackのメイン */
+/* bootpack */
 
 #include "bootpack.h"
 #include <stdio.h>
@@ -10,12 +10,12 @@ void HariMain(void)
 	int mx, my;
 
 	init_gdtidt();
-	init_palette();// 初始化?色板??
+	init_palette();// 初始化?色板
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);//初始化界面背景
 	mx = (binfo->scrnx - 16) / 2;  /* 画面中央坐?的?算 */
 	my = (binfo->scrny - 28 - 16) / 2;
-	init_mouse_cursor8(mcursor, COL8_008484);/*?mcursor???形???色*/
-	/*在特定位置?示一个像素?*/
+	init_mouse_cursor8(mcursor, COL8_008484);
+	/*在特定位置?示一个特定的像素?（?方体）*/
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
